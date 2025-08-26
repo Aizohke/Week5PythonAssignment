8eba3e397cec90ccf36599b419ca62454a5580b5
Aizohke <mathengeisaac04@gmail.com>
2025-08-27 00:04:10 +0300

    Activity 2 answers

---
+++ Activity2.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def move(self):
+        print("Vehicle is moving...")
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving ")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying ")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing ")
+
+# Create objects
+vehicles = [Car(), Plane(), Boat()]
+
+# Demonstrate polymorphism
+for v in vehicles:
+    v.move()
